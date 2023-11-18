20494d568af5b4c998a36b708332d8447f514ff2
Gregor <gregorelcid@proton.me>
2023-11-19 00:26:04 +0100

    added ical data generation

--- randomdatagenerator.py
+++ randomdatagenerator.py
@@ -1,20 +1,61 @@
 import random
+from icalendar import Calendar, Event
+from datetime import datetime, timedelta
+import pytz
 
-# Generating a random instruction
-def generate_instruction():
-    x = random.randint(1, 5)  # Random number between 1-5
-    z = random.randint(1, 120)  # Random number between 1-120
-    y = random.choice(["jumping jacks", "push-ups", "sit-ups", "squats", "lunges", "sunlight viewing", "doomscrolling"])  # Random activity
-    return f"{x} times a week, for {z} minutes, do {y}"
+# Function to generate random protocol instructions
+def generate_protocols(num_instructions, file_path):
+    def generate_instruction():
+        x = random.randint(1, 5)  # Random number between 1-5
+        z = random.randint(1, 120)  # Random number between 1-120
+        y = random.choice(["jumping jacks", "push-ups", "sit-ups", "squats", "lunges", "sunlight viewing", "doomscrolling"])  # Random activity
+        return f"{x} times a week, for {z} minutes, do {y}"
 
-# Number of instructions to generate
-num_instructions = 1000
+    with open(file_path, 'w') as file:
+        for _ in range(num_instructions):
+            instruction = generate_instruction()
+            file.write(instruction + '\n')
+    print(f"Protocol instructions created at {file_path}")
 
-# File path (adjust as needed)
-file_path = 'data/random_protocols.txt'
+# Function to create iCal data
+def create_ical(file_path):
+    def create_event(day):
+        event = Event()
+        event.add('summary', 'Work')
+        event.add('dtstart', day)
+        event.add('dtend', day + timedelta(hours=8))
+        event.add('dtstamp', datetime.now())
+        return event
 
-# Generate and write instructions to a file
-with open(file_path, 'w') as file:
-    for _ in range(num_instructions):
-        instruction = generate_instruction()
-        file.write(instruction + '\n')
+    cal = Calendar()
+    cal.add('prodid', '-//Work Calendar//mxm.dk//')
+    cal.add('version', '2.0')
+
+    start_date = datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
+    if start_date.weekday() != 0:
+        start_date -= timedelta(days=start_date.weekday())
+
+    num_weeks = 4
+    for week in range(num_weeks):
+        for day in range(5):
+            event_day = start_date + timedelta(days=day + week * 7)
+            event = create_event(event_day)
+            cal.add_component(event)
+
+    with open(file_path, 'wb') as f:
+        f.write(cal.to_ical())
+    print(f"iCal file created at {file_path}")
+
+# User input for choice
+choice = input("Enter '1' to generate iCal data or '2' to generate protocol instructions: ")
+
+if choice == '1':
+    ical_file_path = 'Data/work_schedule.ics'
+    create_ical(ical_file_path)
+elif choice == '2':
+    num_instructions = 1000
+    protocol_file_path = 'Data/random_protocols.txt'
+    generate_protocols(num_instructions, protocol_file_path)
+else:
+    print("Invalid choice. Please enter '1' or '2'.")
+
